ffa457fd338cc52f55a2a8a14d704c50b19c1739
IPetelsky <igor.petelsky@gmail.com>
2015-11-03 23:31:55 +0200

    Code refactoring

--- CourseHillel/src/MyMegaProCoolCalculator.java
+++ CourseHillel/src/MyMegaProCoolCalculator.java
@@ -7,17 +7,9 @@ public class MyMegaProCoolCalculator {
 
     public static void main(String[] args) {
         boolean go = true;
+        greetingsMessage();
 
-
-        System.out.println("Hello! This is my first calculator, good luck with it!");
-        System.out.println("For Addition press '+' ");
-        System.out.println("For Subtraction press '-' ");
-        System.out.println("For Multiplication press '*'");
-        System.out.println("For Division press '/' ");
-
-
-        while (go)
-        {
+        while (go) {
             System.out.println("Please choose calculate method");
             Scanner scan = new Scanner(System.in);    //sets up scanners
             Scanner scan1 = new Scanner(System.in);
@@ -26,56 +18,88 @@ public class MyMegaProCoolCalculator {
             String action = scan.nextLine();
 
             {
-                System.out.println("Please enter first number.");
-                double add1 = scan.nextDouble();
-                System.out.println("Please enter second number.");
-                double add2 = scan.nextDouble();
-                double add3 = add1 + add2;
-                System.out.println(add1 + " + " + add2 + " = " + add3);
-            }
-            if ("-".equals(action))
-            {
-                System.out.println("Please enter first number.");
-                int sub1 = scan.nextInt();
-                System.out.println("Please enter second number.");
-                int sub2 = scan.nextInt();
-                int sub3 = sub1 - sub2;
-                System.out.println(sub1 + " - " + sub2 + " = " + sub3);
-            }
-            if ("*".equals(action))
-            {
-                System.out.println("Please enter first number.");
-                int mul1 = scan.nextInt();
-                System.out.println("Please enter second number.");
-                int mul2 = scan.nextInt();
-                int mul3 = mul1 * mul2;
-                System.out.println(mul1 + " + " + mul2 + " = " + mul3);
-            }
-            if ("/".equals(action))
-            {
-                System.out.println("Please enter first number.");
-                int div1 = scan.nextInt();
-                System.out.println("Please enter second number.");
-                int div2 = scan.nextInt();
-                int div3 = div1 / div2;
+                if ("+".equals(action)) {
+                    numOneMsg();
+                    double add1 = scan.nextDouble();
+                    numTwoMsg();
+                    double add2 = scan.nextDouble();
+                    System.out.println(addition(add1, add2));
+                }
+                if ("-".equals(action)) {
+                    numOneMsg();
+                    double sub1 = scan.nextDouble();
+                    numTwoMsg();
+                    double sub2 = scan.nextDouble();
+                    System.out.println(subtraction(sub1, sub2));
+                }
+                if ("*".equals(action)) {
+                    numOneMsg();
+                    double mul1 = scan.nextDouble();
+                    numTwoMsg();
+                    double mul2 = scan.nextDouble();
+                    System.out.println(multiplication(mul1, mul2));
+                }
+                if ("/".equals(action)) {
+                    numOneMsg();
+                    double div1 = scan.nextInt();
+                    numTwoMsg();
+                    double div2 = scan.nextInt();
+                    System.out.println(division(div1, div2));
 
-                System.out.println(div1 + " /" + div2 + " = " + div3);
-
-            }
+                }
 
                 System.out.println("Do you want to start again? (y/n)");
                 String startOver = scan1.nextLine();
                 if ("y".equals(startOver)) {
-                go = true;
+                    go = true;
+                }
                 if ("n".equals(startOver)) {
-                go = false;
-                System.out.println("See ya!");
+                    go = false;
+                    System.out.println("See ya!");
+                }
+
             }
 
-
-            }
         }
+
+
     }
 
+    public static double addition(double one, double two) throws ArithmeticException {
+        return one + two;
+    }
+
+    public static double subtraction(double one, double two) throws ArithmeticException {
+        return one - two;
+    }
+
+    public static double multiplication(double one, double two) throws ArithmeticException {
+        return one * two;
+    }
+
+    public static double division(double one, double two) throws ArithmeticException {
+        return one / two;
+    }
+
+    public static void numOneMsg() {
+        System.out.println("Please enter first number.");
+    }
+
+    public static void numTwoMsg() {
+        System.out.println("Please enter second number.");
+    }
+
+    public static void greetingsMessage() {
+
+        System.out.println("For Addition press '+' ");
+        System.out.println("For Subtraction press '-' ");
+        System.out.println("For Multiplication press '*'");
+        System.out.println("For Division press '/' ");
+
+    }
 }
+
+
+
+
 
